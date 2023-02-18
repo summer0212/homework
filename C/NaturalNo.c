fab52270894aebfcbf7adf4d55c4ef4c5ff8fbf5
summer0212 <sharmaalisha0217@gmail.com>
2023-02-18 23:21:49 +0530

    check for natural no.

---
+++ C/NaturalNo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter a no.\n");
+    scanf("%d", &n);
+    if(n>0)
+    printf("Natural no %d",n);
+    else
+    printf("Not a nautral no %d",n);
+    return 0;
+}
