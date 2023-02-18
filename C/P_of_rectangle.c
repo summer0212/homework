d8fcb00eec7d4671dcf378193122f165851697d4
summer0212 <sharmaalisha0217@gmail.com>
2023-02-18 22:18:10 +0530

    Add a sum program in C

---
+++ C/P_of_rectangle.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+    int l; int b; int p;
+    printf("enter length:\n");
+    scanf("%d", &l);
+    printf("enter breadth:\n");
+    scanf("%d", &b);
+   
+ p=2*(l+b);
+ printf("Perimeter of rectangle = %d units", p);
+ return 0;
+}
+
+
+
