2db9fa2af824cae9f8b8edd25181f3c903725d32
summer0212 <sharmaalisha0217@gmail.com>
2023-02-22 13:36:37 +0530

    Fixed the prime no. series program

--- C/primeNoSeries.c
+++ C/primeNoSeries.c
@@ -6,32 +6,34 @@ int isPrime(int input)
 {
     int i,factor=0;
     for(i=1;i<=input;i++)
-      {
+    {
         if (input%i==0)
-        factor++;
-      }  
+        {
+            factor++;
+        }
+    }  
       
     if(factor==2)
-   return input;
-
+      {
+        return 1;
+      }
+    return 0;
 }    
 
 
 int main()
 {
-int i,input,factor=0;
-printf("enter the number\n");
-scanf("%d", &input);
+    int i,input,factor=0;
+    printf("enter the number\n");
+    scanf("%d", &input);
     for(i=1;i<=input;i++)
     {
-        if(isPrime(input)%i==0 && isPrime(input)!=1) 
-        factor++;
-    }
-if(factor==2)
-    printf("prime no %d", input);
-else
-    printf("not prime %d", input);
-return 0;
+        if(isPrime(i)==1 && i!=1) 
+        {
+            printf("prime no %d\n", i);
+        }
+    }    
+    return 0;
 }
 
 
