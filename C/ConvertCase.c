1d7f250728d1209fe30463719ce5908b73c1ef80
summer0212 <sharmaalisha0217@gmail.com>
2023-03-07 12:15:12 +0530

    Adding conversion from uppercase to lowercase

---
+++ C/ConvertCase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[]="AlishA";
+    int i;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(s[i]>=65 && s[i]<=90)
+        {
+            s[i]=s[i]+32;
+        }
+        else
+        {
+            s[i]=s[i]-32;
+        }
+    }
+    for(i=0;i<strlen(s);i++)
+    {
+        puts(s);
+    }
+    return 0;
+}
